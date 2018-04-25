b9d0656592b0dedc5d65d6d3af5611bf827ac57f
Guilherme Maegava <guilherme-maegava@github.com>
2018-04-25 17:35:20 -0300

    Updates

--- src/list-hero/ListHero.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ListHero from './ListHero';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ListHero/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
